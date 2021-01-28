129620f70a678fe7478adcef5ef20d37a8a7d7c9
lee5031207 <lee5031207@naver.com>
2021-01-28 20:09:34 +0900

    [이성욱]2021.01.28 커밋테스트

--- lets_share/src/com/kh/group/controller/GroupController.java
+++ lets_share/src/com/kh/group/controller/GroupController.java
@@ -7,35 +7,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class GroupController
- */
-@WebServlet("/GroupController")
+@WebServlet("/group/*")
 public class GroupController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public GroupController() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String[] uriArr = request.getRequestURI().split("/");
+		switch (uriArr[uriArr.length-1]) {
+		case "form":
+			goForm(request, response);
+			break;
+		case "upload":
+
+		case "download":
+
+		default : 
+			response.setStatus(404);
+			break;
+		}
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+	
+	protected void goForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/view/group/groupform.jsp")
+		.forward(request, response);
 	}
 
 }
