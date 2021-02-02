4c4777b786d518f8c852aa2858c00f1921233232
lee5031207 <lee5031207@naver.com>
2021-02-02 22:05:55 +0900

    [이성욱] group search, form

--- lets_share/src/com/kh/group/model/dao/GroupDao.java
+++ lets_share/src/com/kh/group/model/dao/GroupDao.java
@@ -38,45 +38,178 @@ public class GroupDao {
 	// 그룹을 만드는 함수 
 	public int insertGroup(Connection conn, Group group) {
 		int res = 0;
+		int res2 = 0;
+		int res3 = 0;
 		PreparedStatement pstm = null;
+		PreparedStatement pstm2 = null;
+		CallableStatement cstm = null;
+		
 		String query = "INSERT INTO SH_GROUP "
 				+ "(GROUP_ID, MB_ID, GROUP_PPL_NUMBER, GROUP_PAYDATE, GROUP_ACCOUNT_INFO, GROUP_SHARE_ID, GROUP_SHARE_PASSWORD, SER_CODE)"
-				+ "VALUES (SC_GROUP_ID.NEXTVAL, ?, 1, ?, ?, ?, ?, ?)";
+				+ "VALUES (SC_GROUP_ID.NEXTVAL, ?, 0, ?, ?, ?, ?, ?)";
+		
+		String query2 = "INSERT INTO SH_MATCHING (MB_ID, GROUP_ID) VALUES (?, SC_GROUP_ID.CURRVAL)";
+		
+		String query3 = "{call PL_UPDATE_MEMBER_CNT(SC_GROUP_ID.CURRVAL)}";
+		
 		try {
 			pstm = conn.prepareStatement(query);
-			pstm.setString(1, "lee5031207");
+			pstm.setString(1, group.getMemberId());
 			pstm.setInt(2, group.getGroupPayDate());
 			pstm.setString(3, group.getAccountInfo());
 			pstm.setString(4, group.getShareId());
 			pstm.setString(5, group.getSharePw());
 			pstm.setString(6, group.getServiceCode());
-			//res = pstm.executeUpdate();
-			procedureMemberCnt(conn, 1020);
+			
+			pstm2 = conn.prepareStatement(query2);
+			pstm2.setString(1, group.getMemberId());
+			
+			cstm = conn.prepareCall(query3);
+			
+			res = pstm.executeUpdate();
+			res2 = pstm2.executeUpdate();
+			res3 = cstm.executeUpdate();
+			
+			System.out.println("res1 : "+res+" / res2 :"+res2+" / res3 : "+res3);
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new DataAccessException(ErrorCode.GR01, e);
 		} finally {
 			jdt.close(pstm);
+			jdt.close(pstm2);
+			jdt.close(cstm);
 		}
 		return res;
 	}
-	
-	// GROUP_ID를 가져오는 함수
-	public ArrayList<Integer> getGroupId(Connection conn, String mbId) {
-		ResultSet rset = null;
-		ArrayList<Integer> groupIds = new ArrayList();
+	public ArrayList<Group> getGroupList(Connection conn){
+		ArrayList<Group> groupList = new ArrayList<Group>();
+		String query = "SELECT * FROM SH_GROUP WHERE GROUP_PPL_NUMBER < 4";
 		PreparedStatement pstm = null;
-		String query = "SELECT GROUP_ID FROM SH_GROUP WHERE MB_ID = ?";
+		ResultSet rset = null;
 		try {
 			pstm = conn.prepareStatement(query);
-			pstm.setString(1, mbId);
 			rset = pstm.executeQuery();
 			while(rset.next()) {
-				groupIds.add(rset.getInt("GROUP_ID"));
+				Group group = new Group();
+				group.setGroupId(rset.getInt(1));
+				group.setMemberId(rset.getString(2));
+				group.setMemberCnt(rset.getInt(3));
+				group.setGroupPayDate(rset.getInt(4));
+				group.setAccountInfo(rset.getString(5));
+				group.setShareId(rset.getString(6));
+				group.setSharePw(rset.getString(7));
+				group.setDate(rset.getDate(8));
+				group.setAutoYN(rset.getString(9).charAt(0));
+				group.setServiceCode(rset.getString(10));
+				group.setMemberCntWish(rset.getInt(11));
+				groupList.add(group);
 			}
 		} catch (SQLException e) {
-			e.printStackTrace();
+			throw new DataAccessException(ErrorCode.SG01, e);
+		} finally {
+			jdt.close(rset,pstm);
 		}
-		return groupIds;
+		return groupList;		
 	}
+	
+	public ArrayList<Group> getGroupList(Connection conn, int date){
+		ArrayList<Group> groupList = new ArrayList<Group>();
+		String query = "SELECT * FROM SH_GROUP WHERE GROUP_PPL_NUMBER < 4 AND GROUP_PAYDATE = ?";
+		PreparedStatement pstm = null;
+		ResultSet rset = null;
+		try {
+			pstm = conn.prepareStatement(query);
+			pstm.setInt(1, date);
+			rset = pstm.executeQuery();
+			while(rset.next()) {
+				Group group = new Group();
+				group.setGroupId(rset.getInt(1));
+				group.setMemberId(rset.getString(2));
+				group.setMemberCnt(rset.getInt(3));
+				group.setGroupPayDate(rset.getInt(4));
+				group.setAccountInfo(rset.getString(5));
+				group.setShareId(rset.getString(6));
+				group.setSharePw(rset.getString(7));
+				group.setDate(rset.getDate(8));
+				group.setAutoYN(rset.getString(9).charAt(0));
+				group.setServiceCode(rset.getString(10));
+				group.setMemberCntWish(rset.getInt(11));
+				groupList.add(group);
+			}
+		} catch (SQLException e) {
+			throw new DataAccessException(ErrorCode.SG01, e);
+		} finally {
+			jdt.close(rset,pstm);
+		}
+		return groupList;		
+	}
+	
+	public ArrayList<Group> getGroupList(Connection conn, String service){
+		ArrayList<Group> groupList = new ArrayList<Group>();
+		String query = "SELECT * FROM SH_GROUP WHERE GROUP_PPL_NUMBER < 4 AND SER_CODE = ?";
+		PreparedStatement pstm = null;
+		ResultSet rset = null;
+		try {
+			pstm = conn.prepareStatement(query);
+			pstm.setString(1, service);
+			rset = pstm.executeQuery();
+			while(rset.next()) {
+				Group group = new Group();
+				group.setGroupId(rset.getInt(1));
+				group.setMemberId(rset.getString(2));
+				group.setMemberCnt(rset.getInt(3));
+				group.setGroupPayDate(rset.getInt(4));
+				group.setAccountInfo(rset.getString(5));
+				group.setShareId(rset.getString(6));
+				group.setSharePw(rset.getString(7));
+				group.setDate(rset.getDate(8));
+				group.setAutoYN(rset.getString(9).charAt(0));
+				group.setServiceCode(rset.getString(10));
+				group.setMemberCntWish(rset.getInt(11));
+				groupList.add(group);
+			}
+		} catch (SQLException e) {
+			throw new DataAccessException(ErrorCode.SG01, e);
+		} finally {
+			jdt.close(rset,pstm);
+		}
+		return groupList;		
+	}
+	
+	public ArrayList<Group> getGroupList(Connection conn, int date, String service){
+		ArrayList<Group> groupList = new ArrayList<Group>();
+		String query = "SELECT * FROM SH_GROUP WHERE GROUP_PPL_NUMBER < 4 AND GROUP_PAYDATE = ? AND SER_CODE = ?";
+		PreparedStatement pstm = null;
+		ResultSet rset = null;
+		try {
+			pstm = conn.prepareStatement(query);
+			pstm.setInt(1, date);
+			pstm.setString(2, service);
+			rset = pstm.executeQuery();
+			while(rset.next()) {
+				Group group = new Group();
+				group.setGroupId(rset.getInt(1));
+				group.setMemberId(rset.getString(2));
+				group.setMemberCnt(rset.getInt(3));
+				group.setGroupPayDate(rset.getInt(4));
+				group.setAccountInfo(rset.getString(5));
+				group.setShareId(rset.getString(6));
+				group.setSharePw(rset.getString(7));
+				group.setDate(rset.getDate(8));
+				group.setAutoYN(rset.getString(9).charAt(0));
+				group.setServiceCode(rset.getString(10));
+				group.setMemberCntWish(rset.getInt(11));
+				groupList.add(group);
+			}
+		} catch (SQLException e) {
+			throw new DataAccessException(ErrorCode.SG01, e);
+		} finally {
+			jdt.close(rset,pstm);
+		}
+		return groupList;		
+	}
+
+	
+	
 }
