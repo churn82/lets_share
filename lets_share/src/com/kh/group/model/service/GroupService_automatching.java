8a8cd779c16b754c96df9f174bf8d27c30843fe4
dpxpshr <dpxpshr@naver.com>
2021-02-06 20:07:11 +0900

    [주민혁]자동매칭트랙잭션

--- lets_share/src/com/kh/group/model/service/GroupService_automatching.java
+++ lets_share/src/com/kh/group/model/service/GroupService_automatching.java
@@ -1,6 +1,8 @@
 package com.kh.group.model.service;
 
 import java.sql.Connection;
+
+import com.kh.common.code.ErrorCode;
 import com.kh.common.exception.DataAccessException;
 import com.kh.common.exception.ToAlertException;
 import com.kh.common.template.JDBCTemplate;
@@ -10,6 +12,31 @@ public class GroupService_automatching {
 
 	GroupDao_automatching groupDao_auto = new GroupDao_automatching();
 	JDBCTemplate jdt = JDBCTemplate.getInstance();
+	//트랜잭션 처리된 함수 : 자동매칭 과정에서 오류가 발생하면, 모두 롤백한다.
+	public int autoMatching(String userSerCode, int userPeriod,String userId) {
+		Connection conn = jdt.getConnection();
+		int groupId = 0;
+		try {
+			groupId = groupDao_auto.findGroup(conn, userSerCode, userPeriod, userId); //조건에 맞는 그룹의 ID를 검색
+			
+			if(groupId == 0) { //조건에 맞는 그룹이 없는 경우 예외 발생시키기
+				throw new DataAccessException(ErrorCode.MR04, new Exception());
+			}
+			groupDao_auto.addGroup(conn, userId, groupId, userPeriod); //찾은 그룹에 유저를 넣어준다.
+			if(groupDao_auto.isMax(conn, groupId)) { //그룹에 유저를 넣어준 후, 그룹이 가득찼다면
+				groupDao_auto.exitQueue(conn, groupId); //그룹을 매칭 대기열에서 제외한다.
+			}
+			jdt.commit(conn); //이상의 작업 중 오류가 발생하지 않으면 커밋한다.
+		}catch (DataAccessException e) {
+			jdt.rollback(conn);
+			groupId = 0; //작업 중 오류 발생시, 롤백하고 그룹id를 0으로 반환하여 오류를 알린다.
+			throw new ToAlertException(e.error);		
+		}finally {
+			jdt.close(conn);
+		}
+		return groupId;
+	}
+	
 	//매칭조건에 맞는 그룹 아이디 검색
 	public int findGroup(String userSerCode, int userPeriod,String userId){
 		
@@ -26,12 +53,10 @@ public class GroupService_automatching {
 	}
 	//그룹아이디와 사용자아이디를 받아 그룹(sh_matching)테이블에 등록
 	public int addGroup(String userId, int groupId, int userPeriod) {
-		
 		Connection conn = jdt.getConnection();
 		int res = 0;
 		try {
 			res = groupDao_auto.addGroup(conn, userId, groupId, userPeriod);
-			jdt.commit(conn);
 		}catch(DataAccessException e) {
 			jdt.rollback(conn);
 			throw new ToAlertException(e.error);
@@ -77,7 +102,6 @@ public class GroupService_automatching {
 		int res = 0;
 		try {
 			res = groupDao_auto.exitQueue(conn, groupId);
-			jdt.commit(conn);
 		}catch(DataAccessException e) {
 			jdt.rollback(conn);
 			throw new ToAlertException(e.error);
